5c568c7aabdc32626792f9c74db6042ce6e81e15
TaylanPostalci <tay.postal@gmail.com>
2022-12-16 15:23:22 -0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :plants , only: [:index , :show , :create]
+  # resources :planes , only: [:index , :create , :destory]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
